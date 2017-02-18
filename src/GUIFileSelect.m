dbadcca7834e1021a272975fd79c2a173ee78603
email <darien.sokolov@yahoo.com>
2017-02-18 10:08:03 -0600

    Added ability to determine potential port coastline locations

--- src/GUIFileSelect.m
+++ src/GUIFileSelect.m
@@ -4,7 +4,7 @@ function [output,boundaries] = GUIFileSelect()
 [FileName,PathName,~] = uigetfile('*','Select an image file.','../imageinput/');
 [output,boundaries] = test(strcat(PathName,FileName)); %Test is a temporary function/file and should be replaced with the final operation.
 
-csvwrite('data/mapCells.dat',output);
+%csvwrite('data/mapCells.dat',output);
 csvwrite('data/mapEdges.dat',boundaries);
 
 %output = jsonencode(output);
