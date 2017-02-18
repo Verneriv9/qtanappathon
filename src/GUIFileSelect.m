566fb42d55768700911cfd1c8367fa0c780ca0a8
email <darien.sokolov@yahoo.com>
2017-02-18 16:22:13 -0600

    Drawmap function

--- src/GUIFileSelect.m
+++ src/GUIFileSelect.m
@@ -1,11 +1,15 @@
 %%%Made in Matlab by Darien Sokolov for the Arcanity group for the Spring Appathon, 2017
 %%%Group Members:  Sarah Austin, Darrel Donald, Darien Sokolov, Verneri Thomason
 
-function [output] = GUIFileSelect()
-
+function [output] = GUIFileSelect(choice)
 [FileName,PathName,~] = uigetfile('*','Select an image file.','../imageinput/');
-[output] = test(strcat(PathName,FileName),8); %Test is a temporary function/file and should be replaced with the final operation.
+if (choice == 1)
+    [output] = test(strcat(PathName,FileName),8); %Test is a temporary function/file and should be replaced with the final operation.
 
-save('data/var','output');
+    save('data/var','output');
+end
+if(choice == 2)
+    [output] = drawMap(strcat(PathName,FileName));
+end
 
 end
---
+++ src/drawMap.m
@@ -0,0 +1,8 @@
+%%%Made in Matlab by Darien Sokolov for the Arcanity group for the Spring Appathon, 2017
+%%%Group Members:  Sarah Austin, Darrel Donald, Darien Sokolov, Verneri Thomason
+
+function mapImage = drawMap(backgroundImage)
+img = imread(backgroundImage);
+imagesc(img);
+
+end
